0f2ec169af732fd07f6b336f439b6d42f8b6d86a
mohdtalib20 <38489681+mohdtalib20@users.noreply.github.com>
2023-07-08 23:36:36 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldfff" );
+        System.out.println( "Hello Worldfff-v1" );
     }
 }
